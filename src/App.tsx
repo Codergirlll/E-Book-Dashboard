fcda7bd337d7ba3906dcd4649c9bbe91d335bdb3
Himani Gangwar <himani.gangwar@w2c.co.in>
2024-05-22 13:43:32 +0530

    shad/cn add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 
 import './App.css'
+import { Button } from "@/components/ui/button"
+
 
 function App() {
 
   return (
     <>
-      <h1>****Welcome to Dashboard ( Himani Gangwar)****</h1>
+
+      <h1> **** Welcome to Dashboard ( Ms G. ) **** </h1>
+
+
+      <div>
+        <Button variant={'destructive'}>Click this button</Button>
+      </div>
 
     </>
   )
